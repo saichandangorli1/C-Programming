3da959af9c293ad0b5011259124672a6a3487585
saichandangorli1 <gorlisaichandan14@gmail.com>
2025-05-12 15:41:37 +0530

    new commit

--- Chap_1_VARIABLES_CONSTANTS _KEYWORDS/data_types.c
+++ Chap_1_VARIABLES_CONSTANTS _KEYWORDS/data_types.c
@@ -5,7 +5,7 @@ int main()
     a = 1;
     // OR
     int a = 1; // Size of the int is usually 4 bytes(32 bits)
-
+    printf(a);
 
     float b;
     b = 1;
@@ -17,7 +17,5 @@ int main()
     // OR
     char c = 'A'; // Size of the char is always 1 bytes(8bits) as per C standard
 
-
-    
     return 0;
 }
